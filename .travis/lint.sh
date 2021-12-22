673d3fdfdb48e9238a897bf0d3a28795a0026d05
Laurent Bouriez <lbouriez@tangerinesoftware.com>
2021-12-22 13:13:49 -0500

    Update

--- .travis/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-docker pull hadolint/hadolint:v1.17.3
-
-echo "now linting: Dockerfile"
-docker run --rm -i hadolint/hadolint:v1.3.0 hadolint --ignore DL3006 - < "$(pwd)/jackett/Dockerfile"
-echo "-------"
